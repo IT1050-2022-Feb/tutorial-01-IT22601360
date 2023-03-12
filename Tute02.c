36c0750afb890f875f863938b5d073d86be0c818
Haritha Himansha <122665975+IT22601360@users.noreply.github.com>
2023-03-12 14:51:21 +0530

    Update Tute02.c
    Here is the C program for the amount to be paid for a rented vehicle.

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,18 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+  float distance, amount;
+
+  printf("Enter the distance : ");
+  scanf("%f", &distance);
+
+  if (distance <= 30) {
+    amount = distance * 50.0;
+  }
+  else{
+    amount = 30 * 50.0 + (distance - 30) * 40.0;
+  }
+
+  printf("Amount : %.2f", amount);
   return 0;
 }
